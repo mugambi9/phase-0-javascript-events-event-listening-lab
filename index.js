c79d703e2c271866eeffbd6176307587c1c1effb
mugambi484 <123549183+mugambi484@users.noreply.github.com>
2024-01-09 22:21:27 +0300

    assignment completed

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
 function addingEventListener() {
 }
+
+const myButton = document.getElementById('myButton');
+
+function handleClick() {
+  console.log('Button clicked!');
+}
+
+myButton.addEventListener('click', handleClick);
+
+    
